acabb004977e4d4fac9c36685172388ca2df5605
Angelina Shamanova <angelinashamanova@gmail.com>
2020-05-03 10:42:42 +0300

    Initial Commit

---
+++ sliderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sliderApp
+//
+//  Created by MacBook on 03/05/2020.
+//  Copyright © 2020 mybestcompany.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
